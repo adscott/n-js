5bf11c100a3ea024a2fb41c96ef541a02f5d6b77
Adam Scott <adam.d.scott@gmail.com>
2012-06-06 13:21:20 +0800

    Made source and minified versions consistent.

--- n.js
+++ n.js
@@ -1,11 +1,11 @@
 var n = function (nsString, register) {
-    var current = window || global;
+    var current = this;
     var pieces = nsString.split('.');
-    
+
     for (var i = 0; i < pieces.length; i++) {
         current[pieces[i]] = current[pieces[i]] || {};
         current = current[pieces[i]];
     }
-    
+
     register(current);
 };
